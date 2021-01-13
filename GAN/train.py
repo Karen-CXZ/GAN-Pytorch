6f7057248df184b33a5da71f3522618ed4242bda
Karen-CXZ <2369549637@qq.com>
2021-01-13 16:02:07 +0800

    modify.

---
+++ GAN/train.py
@@ -0,0 +1,95 @@
+import  os
+import numpy as np
+
+import torch
+
+from torch.autograd import Variable
+from torchvision.utils import save_image
+
+from config import parse_arg
+from dataloader import mnist_loader
+from model import Generator, Discriminator
+
+def train():
+    os.makedirs("image", exist_ok=True)
+    os.makedirs("checkpoints", exist_ok=True)
+
+    cuda = True if torch.cuda.is_available() else False
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+    #  get config and dataloader
+    opt = parse_arg()
+    data_loader = mnist_loader(opt)
+
+    #  Initialize generator and discriminator
+    generator = Generator(opt)
+    discriminator = Discriminator(opt)
+
+    #  Loss function
+    adversarial_loss = torch.nn.BCELoss()
+
+    if cuda:
+        generator.cuda()
+        discriminator.cuda()
+        adversarial_loss.cuda()
+
+    #  Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+    for epoch in range(opt.epochs):
+        for i, (imgs, _) in enumerate(data_loader):
+
+            #  Adversarial ground truths
+            valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+            fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
+
+            #  Configure input
+            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+            real_imgs = Variable(imgs.type(Tensor))
+
+            #  Train Generator
+            #  ---------------
+
+            optimizer_G.zero_grad()
+
+            #  Sample noise as generator input and generate images
+            gen_imgs = generator(z)
+
+            #Loss for generator
+            g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+            # Update parameters
+            g_loss.backward()
+            optimizer_G.step()
+
+            #  Train Discriminator
+            #  -------------------
+
+            optimizer_D.zero_grad()
+            real_loss = adversarial_loss(discriminator(real_imgs), valid)
+            fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+            d_loss = (real_loss+fake_loss)/2
+
+            d_loss.backward()
+            optimizer_D.step()
+
+            #  Log Information
+            #  ---------------
+
+            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" %
+                  (epoch, opt.epochs, i, len(data_loader), d_loss.item(), g_loss.item()))
+
+            batches_done = epoch * len(data_loader) + i
+            if batches_done % opt.sample_interval == 0:
+                save_image(gen_imgs.data[:25], "image/%d.png" % batches_done, nrow=5, normalize=True)
+
+            if batches_done % opt.checkpoint_interval == 0:
+                torch.save(generator.state_dict(), "checkpoints/generator_%d.pth" % epoch)
+
+    torch.save(generator.state_dict(), "checkpoints/generator_done.pth")
+    print("Training Process has been Done!")
+
+
+if __name__ == '__main__':
+    train()
---
+++ WGAN-GP/train.py
@@ -0,0 +1,114 @@
+import  os
+import numpy as np
+
+import torch
+import torch.autograd as autograd
+
+from torch.autograd import Variable
+from torchvision.utils import save_image
+
+from config import parse_arg
+from dataloader import mnist_loader
+from model import Generator, Discriminator
+
+def compute_gradient_penalty(discriminator, real_imgs, gen_imgs, Tensor):
+    epsilon = Tensor(np.random.random((real_imgs.size(0), 1, 1, 1)))
+    interpolates = (epsilon * real_imgs + ((1-epsilon) * gen_imgs)).requires_grad_(True)
+    fake = Variable(Tensor(real_imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+
+    gradients = autograd.grad(outputs=discriminator(interpolates),
+                              inputs=interpolates,
+                              grad_outputs=fake,
+                              create_graph=True,
+                              retain_graph=True,
+                              only_inputs=True)[0]
+    gradients = gradients.view(gradients.size(0), -1)
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+
+    return gradient_penalty
+
+
+
+def train():
+    os.makedirs("image", exist_ok=True)
+    os.makedirs("checkpoints", exist_ok=True)
+
+    cuda = True if torch.cuda.is_available() else False
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+    #  get config and dataloader
+    opt = parse_arg()
+    data_loader = mnist_loader(opt)
+
+    #  Initialize generator and discriminator
+    generator = Generator(opt)
+    discriminator = Discriminator(opt)
+
+    #  Loss function
+
+
+    if cuda:
+        generator.cuda()
+        discriminator.cuda()
+
+
+    #  Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+    for epoch in range(opt.epochs):
+        for i, (imgs, _) in enumerate(data_loader):
+
+            #  Adversarial ground truths
+
+            #  Configure input
+            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+            gen_imgs = generator(z)
+            real_imgs = Variable(imgs.type(Tensor))
+
+            #  Train Generator
+            #  ---------------
+
+            if i % opt.n_critic == 0:
+                optimizer_G.zero_grad()
+
+                # Loss for generator
+                g_loss = -torch.mean(discriminator(gen_imgs))
+
+                # Update parameters
+                g_loss.backward()
+                optimizer_G.step()
+
+            #  Train Discriminator
+            #  -------------------
+
+            optimizer_D.zero_grad()
+            gradient_penalty = compute_gradient_penalty(discriminator, real_imgs.data, gen_imgs.data, Tensor)
+            d_loss = -torch.mean(discriminator(real_imgs))+torch.mean(discriminator(gen_imgs.detach())) + opt.lameda_gp * gradient_penalty
+
+            d_loss.backward()
+            optimizer_D.step()
+
+            #  Clip weights of discriminator
+            #  -----------------------------
+
+
+            #  Log Information
+            #  ---------------
+
+            print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" %
+                  (epoch, opt.epochs, i, len(data_loader), d_loss.item(), g_loss.item()))
+
+            batches_done = epoch * len(data_loader) + i
+            if batches_done % opt.sample_interval == 0:
+                save_image(gen_imgs.data[:25], "image/%d.png" % batches_done, nrow=5, normalize=True)
+
+            if batches_done % opt.checkpoint_interval == 0:
+                torch.save(generator.state_dict(), "checkpoints/generator_%d.pth" % epoch)
+
+    torch.save(generator.state_dict(), "checkpoints/generator_done.pth")
+    print("Training Process has been Done!")
+
+
+if __name__ == '__main__':
+    train()
